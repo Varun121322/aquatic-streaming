62834f1c8f3bde1b4847edfd9d3d949d51354027
Varun Mittal <varun.mittal@duke.edu>
2025-12-02 14:01:41 +0100

    Finished handling message type samples

--- interview/weather.py
+++ interview/weather.py
@@ -2,5 +2,29 @@ from typing import Any, Iterable, Generator
 
 
 def process_events(events: Iterable[dict[str, Any]]) -> Generator[dict[str, Any], None, None]:
-    for line in events:
-        yield line
+    # A dictionary from station name to another dictionary of high: float, low: float
+    stations = {}
+    last_sample_time_stamp = None
+    
+    for message in events:
+        if "type" not in message:
+            raise ValueError("Type field missing in message")
+        
+        message_type = message["type"]
+        if message_type == "sample":
+            try:
+                name = message["stationName"]
+                time_stamp = message["timestamp"]
+                temperature = message["temperature"]
+            except:
+                raise ValueError("Missing field in message")
+            last_sample_time_stamp = time_stamp
+            stats = stations.get(name)
+            if stats is None:
+                stations[name] = {"high": temperature, "low": temperature}
+            else:
+                if temperature > stations[name]["high"]:
+                    stations[name]["high"] = temperature
+                if temperature < stations[name]["low"]:
+                    stations[name]["low"] = temperature
+        
